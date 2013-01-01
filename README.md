d8824003c2c16e5f4cb0cadf54b85858d96ac7c8
gens <gens@heartlandrobotics.com>
2013-01-01 17:37:07 -0500

    add optional ROS MASTER parameter to init script, fix current directory assessment

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ sdk-examples
 ============
 
 SDK Examples
+source init.sh to set up the ros environment
+ - optionally provide the ros master name
+use rosmake joint_pose to make the joint_pose package with dependencies
+
+
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 . /opt/ros/electric/setup.sh
-export ROS_PACKAGE_PATH=$(pwd):${ROS_PACKAGE_PATH}
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+export ROS_PACKAGE_PATH=${DIR}:${ROS_PACKAGE_PATH}
+[[ -n ${1} ]] && export ROS_MASTER_URI=http://${1}:11311
