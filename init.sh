e4b58fafc006db382eb68a45dd97fec61541929c
Justin Bronder <jsbronder@gmail.com>
2013-01-03 13:36:01 -0500

    more helpful init.sh file

--- init.sh
+++ init.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
-. /opt/ros/electric/setup.sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-export ROS_PACKAGE_PATH=${DIR}:${ROS_PACKAGE_PATH}
-[[ -n ${1} ]] && export ROS_MASTER_URI=http://${1}:11311
+
+tf=$(tempfile)
+trap "rm -f -- '${tf}'" EXIT
+
+topdir=$(dirname $(readlink -m ${0}))
+master_uri=""
+
+if [ -n "${1}" ]; then
+	master_uri="${1}"
+fi
+
+cat <<-EOF > ${tf}
+	if [ -s "\${HOME}"/.bashrc ]; then
+		source "\${HOME}"/.bashrc
+	fi
+
+	if ! source /opt/ros/electric/setup.bash; then
+		echo "Failed to source setup for ROS electric"
+		exit 1
+	fi
+
+	export ROS_PACKAGE_PATH=${topdir}:\${ROS_PACKAGE_PATH}
+	[[ -n "${master_uri}" ]] && export ROS_MASTER_URI="http://${master_uri}:11311"
+EOF
+
+${SHELL} --rcfile ${tf}
+
+rm -f -- "${tf}"
+trap - EXIT
+
+# vim: noet
