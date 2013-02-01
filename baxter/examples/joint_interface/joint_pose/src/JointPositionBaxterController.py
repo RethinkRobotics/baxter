e83363003d7f2b000e39e07164f16df99f2d01c9
Gerry Ens <gens@heartlandrobotics.com>
2013-01-31 19:02:45 -0500

    removed record from position controller. added +x to sparse recorder

--- baxter/examples/joint_interface/joint_pose/src/JointPositionBaxterController.py
+++ baxter/examples/joint_interface/joint_pose/src/JointPositionBaxterController.py
@@ -48,36 +48,6 @@ class JointPositionBaxterController(BaxterController):
     self.pubLeftMode.publish(msg)
     self.pubRightMode.publish(msg)
 
-  def timeStamp(self):
-    diff = rospy.Time.now() - self.startTime
-    return diff.to_sec()
-
-  def record(self):
-    """ Records the current joint positions to a csv file
-    if outputFilename was provided at construction
-    this function will record the latest set of joint angles
-    in a csv format.
-    This function does not test to see if a file exists and
-    will overwrite existing files.
-    """
-    if self.outputFilename:
-      if self.newFile:
-        with open(self.outputFilename, 'w') as f:
-          f.write('time,')
-          f.write(','.join(self.leftPosition.keys()) + ',')
-          f.write('left_gripper,')
-          f.write(','.join(self.rightPosition.keys()) + ',')
-          f.write('right_gripper\n')
-        self.newFile = False
-
-      with open(self.outputFilename, 'a') as f:
-        f.write("%f," % (self.timeStamp(),))
-        f.write(','.join([str(x) for x in self.leftPosition.values()]) + ',')
-        f.write(str(self.gripperLeft.position) + ',')
-        f.write(','.join([str(x) for x in self.rightPosition.values()]) + ',')
-        f.write(str(self.gripperRight.position) + '\n')
-
-
   def leftJointState(self, data):
     """ callback function for the ROS subscriber of the left joint angles
     Args:
