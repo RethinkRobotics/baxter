7bca73e1ed0f5beca464eed79930a89533b54b14
Gerry Ens <gens@heartlandrobotics.com>
2013-02-01 19:16:25 -0500

    more refactoring, putting mappers and controllers in the same module. May end up splitting those out again in packages...

--- baxter/examples/joint_interface/joint_pose/src/controllers.py
+++ baxter/examples/joint_interface/joint_pose/src/controllers.py
@@ -2,12 +2,52 @@ import roslib
 roslib.load_manifest('joint_pose')
 import rospy
 
-from BaxterController import BaxterController
-from GripperBaxterController import GripperBaxterController
-
+from std_msgs.msg import (Empty, Float32,)
 from sensor_msgs.msg import JointState
-from baxter_msgs.msg import JointCommandMode
-from baxter_msgs.msg import JointPositions
+from baxter_msgs.msg import (GripperState, JointCommandMode, JointPositions,)
+
+class BaxterController(object):
+  """ Interface for a Baxter robot controller
+  """
+
+  def command(self, commands):
+    """ generic command function
+    Args:
+      commands(dict{str:float}): a set of name/value pairs
+        that forms a robot command
+    """
+    raise NotImplementedError()
+
+class GripperBaxterController(BaxterController):
+  """ Controls a gripper on a Baxter Robot
+  """
+  def __init__(self, arm):
+    self.pubCalibrate = rospy.Publisher('/robot/limb/' + arm + '/accessory/gripper/command_calibrate', Empty)
+    self.pubGoto = rospy.Publisher('/robot/limb/' + arm + '/accessory/gripper/command_goto', Float32)
+    self.subState = rospy.Subscriber('/robot/limb/' + arm + 'accessory/gripper/state', GripperState, self.gripperState)
+    self.arm = arm
+    self.calibrated = False
+    self.position = 100.0 #open
+    self.force = 0.0
+
+  def gripperState(self, msg):
+    self.position = msg.position
+    self.force = msg.force
+    self.calibrated = msg.calibrated
+
+  def command(self, commands):
+    if not self.calibrated:
+      print("Calibrating %s gripper" % (self.arm,))
+      self.pubCalibrate.publish(Empty())
+    else:
+      self.calibrating = False
+      if 'position' in commands:
+        position = commands['position']
+        self.pubGoto.publish(Float32(position))
+        self.position = position
+      else:
+        print("gripper %s does not know how to handle command(s) '%s'. Ignored" % (self.arm, str(commands.keys())))
+
 
 class JointPositionBaxterController(BaxterController):
   """ Joint Position Controller for a Rethink Robotics Baxter RSDK Robot
@@ -35,7 +75,6 @@ class JointPositionBaxterController(BaxterController):
     self.subRight = rospy.Subscriber('/robot/limb/right/joint_states', JointState, self.rightJointState)
     self.gripperLeft = GripperBaxterController("left")
     self.gripperRight = GripperBaxterController("right")
-    self.outputFilename = None#outputFilename
     self.newFile = True
     self.startTime = rospy.Time.now()
 
