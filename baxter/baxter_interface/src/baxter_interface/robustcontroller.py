070467df3c5be0f062526e13a136ccb2f3a5cb0b
Justin Bronder <jbronder@rethinkrobotics.com>
2013-02-05 11:28:47 -0500

    baxter_interface:  initial commit
    Add the baxter_interface stack which will contain a minimal framework
    for working with Baxter via python.
    
    Initial commit contains a shim class for running RobustControllers.
    
    Also added the RobustControllerStatus message to baxter_msgs.

---
+++ baxter/baxter_interface/src/baxter_interface/robustcontroller.py
@@ -0,0 +1,107 @@
+import roslib
+roslib.load_manifest('baxter_interface')
+import rospy
+
+from baxter_msgs.msg import (
+    RobustControllerStatus,
+)
+
+class RobustController(object):
+    STATE_IDLE = 0
+    STATE_STARTING = 1
+    STATE_RUNNING = 2
+    STATE_STOPPING = 3
+
+    def __init__(self, namespace, enable_msg, disable_msg, timeout = 60):
+        """
+        Wrapper around controlling a RobustController
+
+        @param namespace    - namespace containing the enable and status topics
+        @param enable_msg   - message to send to enable the RC
+        @param disable_msg  - message to send to disable the RC
+        @param timeout      - seconds to wait for the RC to finish [60]
+        """
+        self._command_pub = rospy.Publisher(
+            namespace + '/enable',
+            type(enable_msg))
+        self._status_sub = rospy.Subscriber(
+            namespace + '/status',
+            RobustControllerStatus,
+            self._callback)
+
+        self._enable_msg = enable_msg
+        self._disable_msg = disable_msg
+        self._timeout = timeout
+        self._state = self.STATE_IDLE
+        self._return = (0, "Success")
+
+        rospy.on_shutdown(self._on_shutdown)
+
+    def _callback(self, msg):
+        if self._state == self.STATE_RUNNING:
+            if msg.complete == RobustControllerStatus.COMPLETE_W_SUCCESS:
+                self._return = (0, "Success")
+                self._state = self.STATE_STOPPING
+
+            elif msg.complete == RobustControllerStatus.COMPLETE_W_FAILURE:
+                self._state = self.STATE_STOPPING
+                self._return = (1, msg.errorMsg)
+
+            elif not msg.isEnabled:
+                self._state = self.STATE_IDLE
+                self._return = (1, msg.errorMsg)
+
+        elif self._state == self.STATE_STOPPING and not msg.isEnabled:
+            # Would be nice to use msg.state here, but it does not
+            # consistently reflect reality.
+            self._state = self.STATE_IDLE
+
+        elif self._state == self.STATE_STARTING and msg.isEnabled:
+            self._state = self.STATE_RUNNING
+
+    def _run_loop(self):
+        # RobustControllers need messages at > 1Hz in order to continue
+        # their current operation.
+        rate = rospy.Rate(1.1)
+        start = rospy.Time.now()
+
+        while not rospy.is_shutdown():
+            if self._state == self.STATE_RUNNING and (rospy.Time.now() - start).to_sec() > self._timeout:
+                self._state = self.STATE_STOPPING
+                self._command_pub.publish(self._disable_msg)
+                self._return = (1, "RobustController did not finish after %d seconds" % (self._timeout,))
+
+            elif self._state in (self.STATE_STARTING, self.STATE_RUNNING):
+                self._command_pub.publish(self._enable_msg)
+
+            elif self._state == self.STATE_STOPPING:
+                self._command_pub.publish(self._disable_msg)
+
+            elif self._state == self.STATE_IDLE:
+                break
+
+            rate.sleep()
+
+    def _on_shutdown(self):
+        rate = rospy.Rate(1.1)
+
+        while not self._state == self.STATE_IDLE:
+            print "sending shutdown"
+            self._command_pub.publish(self._disable_msg)
+            rate.sleep()
+
+        self._return = (1, "Operation cancelled by user")
+
+    def run(self):
+        """
+        Enable the RobustController and run until completion or error.
+
+        @returns    - Tuple(int,str).  Returns the exit code and error string
+                      from the robust controller.
+        """
+        self._state = self.STATE_STARTING
+        self._command_pub.publish(self._enable_msg)
+        self._run_loop()
+        return self._return
+
+
