adf67996fa22ec161b464110ab70a8b8f8da2b5b
Rob Linsalata <rlinsalata@rethinkrobotics.com>
2013-03-06 18:54:06 -0500

    DigitalIO: updating baxter_interface __init__.py

--- baxter/baxter_interface/src/baxter_interface/__init__.py
+++ baxter/baxter_interface/src/baxter_interface/__init__.py
@@ -2,6 +2,7 @@ from robot_enable import RobotEnable
 from gripper import Gripper
 from head import Head
 from limb import Limb
+from digital_io import DigitalIO
 from settings import (
     JOINT_ANGLE_TOLERANCE,
 )
