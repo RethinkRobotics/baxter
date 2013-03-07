a1d25cbda7db6c089b1c46e9d81bd29a21412e2d
Rob Linsalata <rlinsalata@rethinkrobotics.com>
2013-03-07 16:52:49 -0500

    AnalogIO: cleaning up, per code review.

--- baxter/baxter_interface/src/baxter_interface/analog_io.py
+++ baxter/baxter_interface/src/baxter_interface/analog_io.py
@@ -1,3 +1,5 @@
+import errno
+
 import roslib
 roslib.load_manifest('baxter_interface')
 import rospy
@@ -21,35 +23,34 @@ class AnalogIO(object):
         """
         self._id = component_id
         self._component_type = 'analog_io'
-        self._is_output = None
+        self._can_output = False
+
         self._state = {}
 
-        type_ns = '/robot/' + self._component_type
+        type_ns = '/sdk/robot/' + self._component_type
         topic_base = type_ns + '/' + self._id
 
         self._sub_state = rospy.Subscriber(
-            '/sdk' + topic_base + '/state',
+            topic_base + '/state',
             AnalogIOState,
             self._on_io_state)
 
-        rate = rospy.Rate(100)
         while not rospy.is_shutdown():
             if len(self._state):
                 break
-            rate.sleep()
+            rospy.sleep(0.01)
 
         # check if output-capable before creating publisher
-        if self._is_output:
+        if self._can_output:
             self._pub_output = rospy.Publisher(
-                '/sdk' + type_ns + '/command',
+                type_ns + '/command',
                 AnalogOutputCommand)
 
     def _on_io_state(self, msg):
         """
         Updates the internally stored state of the Analog Input/Output.
         """
-        if self._is_output == None:
-            self._is_output = not msg.isInputOnly
+        self._can_output = not msg.isInputOnly
         self._state['value'] = msg.value
 
     def state(self):
@@ -64,10 +65,9 @@ class AnalogIO(object):
 
         @param value uint16    - new state of the Output.
         """
-        if not self._is_output:
-            rospy.logerr("Component is not an output [%s: %s]" % (
-                self._component_type, self._id))
-            return
+        if not self._can_output:
+            raise IOError(errno.EACCES, "Component is not an output [%s: %s]" %
+                (self._component_type, self._id))
         m_cmd = AnalogOutputCommand()
         m_cmd.name = self._id
         m_cmd.value = value
