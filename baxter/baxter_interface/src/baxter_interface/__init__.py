8d9fa0158b70499a192e5180d95557173a27c41d
Rob Linsalata <rlinsalata@rethinkrobotics.com>
2013-03-06 19:21:15 -0500

    AnalogIO: updating baxter_interface __init__.py

--- baxter/baxter_interface/src/baxter_interface/__init__.py
+++ baxter/baxter_interface/src/baxter_interface/__init__.py
@@ -3,6 +3,7 @@ from gripper import Gripper
 from head import Head
 from limb import Limb
 from digital_io import DigitalIO
+from analog_io import AnalogIO
 from settings import (
     JOINT_ANGLE_TOLERANCE,
 )
