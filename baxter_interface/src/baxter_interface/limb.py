701f823fccba5559e5e8ab6d1689c18cea650687
Rob Linsalata <rlinsalata@rethinkrobotics.com>
2013-10-30 16:46:17 -0400

    minor: adding capitalize in err msg

--- baxter_interface/src/baxter_interface/limb.py
+++ baxter_interface/src/baxter_interface/limb.py
@@ -101,10 +101,10 @@ class Limb(object):
             JointState,
             self._on_joint_states)
 
-        init_err_msg = ("%s limb init failed to get current joint_states "
-                        "from %s") % (self.name, joint_state_topic)
+        err_msg = ("%s limb init failed to get current joint_states "
+                   "from %s") % (self.name.capitalize(), joint_state_topic)
         dataflow.wait_for(lambda: len(self._joint_angle.keys()) > 0,
-                          timeout_msg=init_err_msg)
+                          timeout_msg=err_msg)
 
     def _on_joint_states(self, msg):
         for idx, name in enumerate(msg.name):
@@ -322,7 +322,7 @@ class Limb(object):
                          for diff in diffs)),
             timeout=timeout,
             timeout_msg=("%s limb failed to reach commanded joint positions" %
-                         (self.name,)),
+                         (self.name.capitalize(),)),
             rate=100,
             body=lambda: self.set_joint_positions(positions)
             )
